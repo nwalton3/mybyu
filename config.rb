acee676290a7105944a5f7682a9ac3b221d01bb0
Nathan Walton <nwalton@byu.edu>
2012-10-03 14:56:26 -0600

    Add base files

---
+++ config.rb
@@ -0,0 +1,24 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "uPortal/media/skins/universality/byu"
+sass_dir = "uPortal/media/skins/universality/byu/scss"
+images_dir = "uPortal/media/skins/universality/byu/images"
+javascripts_dir = "js"
+
+output_style = :compressed
+environment = :development
+
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass css-sass scss && rm -rf sass && mv scss sass
